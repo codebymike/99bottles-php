c53a95fb8e89a60e980f8a3ceb08dfb565c6a381
Mike Thomas <mikethomas84@gmail.com>
2025-01-13 15:23:01 +0000

    3 passing tests

--- 99bottles_php/lib/Bottles.php
+++ 99bottles_php/lib/Bottles.php
@@ -7,9 +7,11 @@ class Bottles
         $number = (string) $number;
         $minusOne = (string) ($number - 1);
 
+        $lastContainer = $minusOne == 1 ? 'bottle' : 'bottles';
+
         return "{$number} bottles of beer on the wall, " .
             "{$number} bottles of beer.\n" .
             "Take one down and pass it around, " .
-            "{$minusOne} bottles of beer on the wall.\n";
+            "{$minusOne} {$lastContainer} of beer on the wall.\n";
     }
 }
