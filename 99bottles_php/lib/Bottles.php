370fe22213f38ff1141e96d204486038d79b3bd3
Mike Thomas <mikethomas84@gmail.com>
2025-01-21 14:48:16 +0000

    refactor complete

--- 99bottles_php/lib/Bottles.php
+++ 99bottles_php/lib/Bottles.php
@@ -43,10 +43,10 @@ class Bottles
                     "no more bottles of beer on the wall.\n";
             default:
                 return
-                    $number ." bottles of beer on the wall, " .
-                    $number . " bottles of beer.\n" .
+                    $number ." ". $this->container( $number ) ." of beer on the wall, " .
+                    $number ." ". $this->container( $number ) ." of beer.\n" .
                     "Take one down and pass it around, " .
-                    ($number - 1) . " bottles of beer on the wall.\n";
+                    ($number - 1) . " ". $this->container( $number - 1 ) ." of beer on the wall.\n";
         }
     }
 }
