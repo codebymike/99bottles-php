da45b2352e24cafc15538fcfe3b72599e8bdca93
Mike Thomas <mikethomas84@gmail.com>
2025-01-30 16:24:27 +0000

    reimplmeneting next

--- 99bottles_php/lib/Bottles.php
+++ 99bottles_php/lib/Bottles.php
@@ -53,7 +53,7 @@ class Bottles
             ucfirst( $bottleNumber->quantity() ) ." ". $bottleNumber->container() ." of beer on the wall, " .
             $bottleNumber->quantity() . " " . $bottleNumber->container() . " of beer.\n" .
             $bottleNumber->action() . ", " .
-            $bottleNumber->next()->quantity() ." ". $bottleNumber->next->()->container() ." of beer on the wall.\n";
+            $bottleNumber->next()->quantity() ." ". $bottleNumber->next()->container() ." of beer on the wall.\n";
     }
 }
 
